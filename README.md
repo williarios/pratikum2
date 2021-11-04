a7e191b13d641ce2e5384ed56c5e90a3b20929d3
williarios <williarios771@gmail.com>
2021-11-04 21:51:52 +0700

    luas dan keliling lingkaran

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # pratikum2
+Luas Lingkaran
+
+Diketahui = a. phi= 3,14
+	    b. r = 14
+
+Ditanya = Luas Lingkaran ?
+Jawab = phi*r*r
+      = 3,14*14*14
+      = 615.44
+
+Keliling Lingkaran
+
+Diketahui = a. phi= 3,14
+	  = b. r = 7
+
+Ditanya = Keliling Lingkaran ?
+Jawab = 2*phi*r
+      = 2*3,14*7
+      = 43.96
---
+++ luaslingakaran/lingkaran.py
@@ -0,0 +1,17 @@
+#nama saya
+print ( "program menghitung luas lingkaran dan keliling lingkaran \n " )
+print ( "nama elia william aritonang \ nNIm:312110456" )
+print ( "tanggal pengerjaan tugas 4 november 2021 \n " )
+
+#menghitung luas lingkaran
+phi  =  3.14
+r  =  float ( input ( " masukan panjang jari jari lingkaran : " ))
+luas_lingkaran  =  phi * r * r
+
+print ( "luas lingkaran adalah :" , luas_lingkaran )
+
+#menghitung keliling lingkaran
+r  =  float ( input ( " masukan jari jari lingkaran :" ))
+keliling_lingkaran  =  2 * phi * r
+
+print ( "keliling lingkaran adalah :" , keliling_lingkaran )
